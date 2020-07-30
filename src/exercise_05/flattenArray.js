b93484f5ab0cdc665124d43696143c02fa776ac0
ghjhh <798389920@qq.com>
2020-07-30 23:00:32 +0800

    [WangZuowen]feat:Finish flattenArray.js.

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -4,7 +4,25 @@ export default function flattenArray(array) {
   // Your target:
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
-  // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  // * Please do NOT modify the signature of the function.  
+  if (array === undefined || array === null) {
+    throw new Error('Flatten undefined or null array');
+  }
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].length !== 0 && array[i].length !== undefined) {
+      for (let itemIndex = 0; itemIndex < array[i].length; itemIndex++) {
+        if (result.indexOf(array[i][itemIndex]) !== -1) {
+          continue;
+        }
+        result.push(array[i][itemIndex]);
+      }
+    } else {
+      if (result.indexOf(array[i]) !== -1) {
+        continue;
+      }
+      result.push(array[i]);
+    }
+  }
+  return result;
 }
