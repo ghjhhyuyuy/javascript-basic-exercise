4fe1df238b30288f9736e6319397981b9b556152
ghjhh <798389920@qq.com>
2020-07-30 22:34:24 +0800

    [WangZuowen]feat:Finish arrayToList.js.

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -5,6 +5,21 @@ export default function arrayToList(array) {
   //
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (array === null || array === undefined) {
+    throw new Error('Creating list from undefined array');
+  }
+  if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  }
+  const itemArray = [];
+  for (let i = 0; i < array.length; i++) {
+    const item = {};
+    item.value = array[i];
+    item.next = null;
+    itemArray.push(item);
+  }
+  for (let i = 0; i < itemArray.length - 1; i++) {
+    itemArray[i].next = itemArray[i + 1];
+  }
+  return itemArray[0];
 }
